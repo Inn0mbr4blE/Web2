8698fb2962f90cdf8c63ef9415dd0607cdc1ba07
BOG-A209-E-025 <BOG-A209-E-025@RGH-LS6-25.UMD.LOCAL.EDU>
2019-02-07 21:28:11 -0500

    Incluir una librería maven

---
+++ ProgramacionWeb/src/main/java/com/progwe/maven/inicio.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.progwe.maven;
+
+import java.time.LocalTime;
+
+/**
+ *
+ * @author BOG-A209-E-025
+ */
+public class inicio {
+    public static void main(String[] args) {
+        LocalTime tiempo = new LocalTime();
+        System.out.println("El tiempo es":+tiempo);
+    }
+    
+}
